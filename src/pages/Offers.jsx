c6c35979cd5ffe57f8136f3c1b1403e47c478414
alankar-ttn <alankar.saxena@tothenew.com>
2022-03-14 12:35:27 +0530

    removed unused imports

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -7,7 +7,6 @@ import {
 	where,
 } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import ListingItem from "../components/ListingItem";
 import Spinner from "../components/Spinner";
